f8d259b9ed40990cd6f18a35aa639144c663e3c8
Prof. Scott F. H. Kaplan <sfkaplan@cs.amherst.edu>
2013-05-23 21:00:48 -0400

    A bit of a beginning.

---
+++ kernel/kvmtrace.c
@@ -0,0 +1,13 @@
+/**
+ * \file kvmtrace.c
+ * \author Scott F. H. Kaplan <sfkaplan@cs.amherst.edu>
+ * \date May 2013
+ *
+ * The core kVMTrace module.  It stores the central state and provides
+ * the controls as to whether reference logging is active, among other
+ * basics.
+ */
+
+/**
+ * 
+ */
